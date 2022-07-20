1cae623510d77027eb136c1b7d18e64088747035
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-07-19 23:49:30 -0400

    Make `--help` reflect current update

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,21 @@ fn main() {
 	let args: Vec<String> = std::env::args().collect();
 	if args.len() < 2 || args.iter().any(|a| a == "--help") || args.iter().any(|a| a == "-h") {
 		println!("\
-			usage: collatz [n]\n\
-			n must be an integer decimal (B10) numeral\n\
-			use 0x prefix to input hexadecimal (B16), or 0t for ternary (B3)\
+			Usage: collatz_finder [n]\n\
+			n must be an integer numeral.\n\
+			Add a prefix to specify the base/radix (optional).\n\
+			Supported prefixes and their associated radices:\n\
+			0u (unary, 1)\n\
+			0b (binary, 2)\n\
+			0t (ternary, 3)\n\
+			0q (quaternary, 4)\n\
+			0p (pental/quinary, 5)\n\
+			0h (heximal/senary, 6. NOT hexaDECimal)\n\
+			0s (septimal/septenary, 7)\n\
+			0o (octal, 8. the \"o\" is mandatory)\n\
+			0n (nonary, 9)\n\
+			0d (decimal, 10. standard de facto)\n\
+			0x (hexadecimal, 16)\
 		");
 		return;
 	}
